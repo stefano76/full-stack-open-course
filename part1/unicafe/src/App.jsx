b38de6ae92237e62f87c6b77be67f384467a57f9
stefano76 <stefano.bonuccelli@gmail.com>
2026-08-24 09:26:26 +1200

    Exercise 1.6: Created app unicafe for updating the statistics

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+const Header = () => {
+	return <h1>Give us your feedback</h1>;
+};
+
+const Button = ({ text, onClick }) => {
+	return (
+		<button onClick={onClick} style={{ marginRight: "1rem" }}>
+			{text}
+		</button>
+	);
+};
+
+const Statistics = ({ text, number }) => {
+	return (
+		<li>
+			{text}: {number}
+		</li>
+	);
+};
+
+const App = () => {
+	// save clicks of each button to its own state
+	const [good, setGood] = useState(0);
+	const [neutral, setNeutral] = useState(0);
+	const [bad, setBad] = useState(0);
+
+	const handleGood = () => setGood(good + 1);
+	const handleNeutral = () => setNeutral(neutral + 1);
+	const handleBad = () => setBad(bad + 1);
+
+	const feedbacks = {
+		good: good,
+		neutral: neutral,
+		bad: bad
+	};
+
+	// console.log(feedbacks);
+
+	return (
+		<div>
+			<Header />
+			<div>
+				<Button text="Good" onClick={handleGood} />
+				<Button text="Neutral" onClick={handleNeutral} />
+				<Button text="Bad" onClick={handleBad} />
+			</div>
+			<h2>Statistics</h2>
+			<ul>
+				<Statistics text="Good" number={feedbacks.good} />
+				<Statistics text="Neutral" number={feedbacks.neutral} />
+				<Statistics text="Bad" number={feedbacks.bad} />
+			</ul>
+		</div>
+	);
+};
+
+export default App;
