d87f38796e74930f58b84783a034a2cf14f55599
stefano76 <stefano.bonuccelli@gmail.com>
2026-08-25 11:52:15 +1200

    Exerices 2.9: The Phonebook Step 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,14 @@ import {useState} from 'react'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        {name: 'Arto Hellas', number: '01-2345-6789'}
+        {name: 'Arto Hellas', number: '040-123456', id: 1},
+        {name: 'Ada Lovelace', number: '39-44-5323523', id: 2},
+        {name: 'Dan Abramov', number: '12-43-234345', id: 3},
+        {name: 'Mary Poppendieck', number: '39-23-6423122', id: 4}
     ])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
+    const [filteredPersons, setFilteredPersons] = useState([...persons])
 
     const addName = (event) => {
         event.preventDefault()
@@ -30,6 +34,14 @@ const App = () => {
         setNewNumber(event.target.value)
     }
 
+    const handleSearch = (event) => {
+        // console.log(event.target.value)
+
+        setFilteredPersons(
+            persons.filter(person => person.name.toLowerCase().includes(event.target.value.toLowerCase()))
+        )
+    }
+
     const Number = ({person}) => {
         return (
             <li>{person.name}: {person.number}</li>
@@ -39,19 +51,22 @@ const App = () => {
     return (
         <div>
             <h2>Phonebook</h2>
+            <div>Filter shown names: <input onChange={handleSearch}/></div>
+
+            <h2>Add a new person</h2>
             <form onSubmit={addName}>
                 <div style={{display: 'flex', flexDirection: 'column', gap: '1rem'}}>
                     <div>name: <input value={newName} onChange={handleNewName}/></div>
                     <div>number: <input value={newNumber} onChange={handleNewNumber}/></div>
                     <div>
-                        <button type="submit">Add new person</button>
+                        <button type="submit">Add</button>
                     </div>
                 </div>
             </form>
             <h2>Numbers</h2>
             <ul>
-                {persons.map((person) => (
-                    <Number key={person.name.replaceAll(" ", "")} person={person}/>
+                {filteredPersons.map((person) => (
+                    <Number key={person.id} person={person}/>
                 ))}
             </ul>
         </div>
