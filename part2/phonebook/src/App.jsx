d0dcf06a615081a5dc1d6caaee81cb6b61d05179
stefano76 <stefano.bonuccelli@gmail.com>
2026-08-25 10:31:03 +1200

    Exercise 2.6: The Phonebook step 1

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,26 +1,47 @@
-import { useState } from 'react'
+import {useState} from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ])
-  const [newName, setNewName] = useState('')
+    const [persons, setPersons] = useState([
+        {name: 'Arto Hellas'}
+    ])
+    const [newName, setNewName] = useState('')
 
-  return (
-      <div>
-        <h2>Phonebook</h2>
-        <form>
-          <div>
-            name: <input />
-          </div>
-          <div>
-            <button type="submit">add</button>
-          </div>
-        </form>
-        <h2>Numbers</h2>
-        ...
-      </div>
-  )
+    // console.log(persons)
+
+    const addName = (event) => {
+        event.preventDefault()
+        console.log(newName)
+
+        setPersons([...persons, {name: newName}])
+        setNewName('')
+    }
+
+    const handleNewName = (event) => {
+        // console.log(event.target.value)
+        setNewName(event.target.value)
+    }
+
+    const Number = ({ person }) => <li>{person.name}</li>
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={addName}>
+                <div>
+                    name: <input value={newName} onChange={handleNewName} />
+                </div>
+                <div>
+                    <button style={{ marginTop: '1rem' }} type="submit">Add new name</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            <ul>
+                {persons.map((person) => (
+                    <Number key={person.name.replaceAll(" ", "")} person={person} />
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default App
