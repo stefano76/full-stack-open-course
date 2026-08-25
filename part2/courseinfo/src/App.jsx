9c60c4fb6acf682c941b05843aadaf4a85e00ceb
stefano76 <stefano.bonuccelli@gmail.com>
2026-08-25 15:30:00 +1200

    Updated courseinfo

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,60 +1,21 @@
-import Course from './components/Course'
+import { useState, useEffect } from "react"
+import axios from "axios"
 
 const App = () => {
-    const courses = [
-        {
-            name: 'Half Stack application development',
-            id: 1,
-            parts: [
-                {
-                    name: 'Fundamentals of React',
-                    exercises: 10,
-                    id: 1
-                },
-                {
-                    name: 'Using props to pass data',
-                    exercises: 7,
-                    id: 2
-                },
-                {
-                    name: 'State of a component',
-                    exercises: 14,
-                    id: 3
-                },
-                {
-                    name: 'Redux',
-                    exercises: 11,
-                    id: 4
-                }
-            ]
-        },
-        {
-            name: 'Node.js',
-            id: 2,
-            parts: [
-                {
-                    name: 'Routing',
-                    exercises: 3,
-                    id: 1
-                },
-                {
-                    name: 'Middlewares',
-                    exercises: 7,
-                    id: 2
-                }
-            ]
-        }
-    ]
+    const [notes, setNotes] = useState([])
+    const [newNote, setNewNote] = useState('')
+    const [showAll, setShowAll] = useState(true)
 
-    return (
-        <div>
-            <h1>Web development curriculum</h1>
-
-            {courses.map((course) => (
-                <Course key={course.id} course={course} />
-            ))}
-        </div>
-    )
+    useEffect(() => {
+        console.log('effect')
+        axios
+            .get('http://localhost:3001/notes')
+            .then(response => {
+                console.log('promise fulfilled')
+                setNotes(response.data)
+            })
+    }, [])
+    console.log('render', notes.length, 'notes')
 }
 
 export default App
