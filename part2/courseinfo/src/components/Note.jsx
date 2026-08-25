8a30a1a123c49bbe815cd988e2ea2f0d3f13b61e
stefano76 <stefano.bonuccelli@gmail.com>
2026-08-25 17:22:18 +1200

    updated courseinfo

--- part2/courseinfo/src/components/Note.jsx
+++ part2/courseinfo/src/components/Note.jsx
@@ -1,5 +1,12 @@
-const Note = ({ note }) => {
-    return <li>{note.content}</li>
+const Note = ({ note, toggleImportance }) => {
+    const label = note.important ? 'make not important' : 'make important'
+
+    return (
+        <li style={{ marginBottom: '.5rem' }}>
+            {note.content}
+            <button style={{marginLeft: '1rem'}} onClick={toggleImportance}>{label}</button>
+        </li>
+    )
 }
 
 export default Note
