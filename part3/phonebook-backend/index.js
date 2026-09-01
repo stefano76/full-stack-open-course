2ef0faa2fc26547a46e515e84d185e74f1bf34a6
stefano76 <stefano.bonuccelli@gmail.com>
2026-09-02 10:25:44 +1200

    Exercise 3.7: Phonebook backend step 7

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
+// morgan('tiny')
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let persons = [
     {
