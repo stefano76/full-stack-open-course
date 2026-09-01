a2e5075beb65e2bc96789c32195b5bd598da385d
stefano76 <stefano.bonuccelli@gmail.com>
2026-09-01 17:48:54 +1200

    Exercise 3.4: Phonebook backend step 4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const persons = [
+let persons = [
     {
         "id": "1",
         "name": "Arto Hellas",
@@ -44,6 +44,13 @@ app.get('/api/persons/:id', (request, response) => {
     } else {
         response.status(404).end()
     }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id;
+    persons = persons.filter(p => p.id !== id)
+
+    response.status(204).end()
 })
 
 const PORT = 3001
