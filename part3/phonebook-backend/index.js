49a82f9fdf1e0f2130ba8f73706bcdf839959824
stefano76 <stefano.bonuccelli@gmail.com>
2026-09-01 17:44:10 +1200

    Exercise 3.3: Phonebook backend step 3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -35,5 +35,16 @@ app.get('/info', (request, response) => {
     response.send(output)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id;
+    const person = persons.find(p => p.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
 const PORT = 3001
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
