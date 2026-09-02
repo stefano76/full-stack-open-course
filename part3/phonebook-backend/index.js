6cb7086ddb68f0cd9e031f9067ede7d609c50515
stefano76 <stefano.bonuccelli@gmail.com>
2026-09-02 18:46:34 +1200

    Exercise 3.11 Full Stack Phonebook

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
 app.use(express.static('dist'))
 app.use(morgan('tiny'))
-app.use(cors())
 
 let persons = [
     {
