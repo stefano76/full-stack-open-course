4212a3ee7cbd46d5967187609461783f417bbfcf
stefano76 <stefano.bonuccelli@gmail.com>
2026-09-03 17:21:42 +1200

    Exercise 3.15: Phonebook database, step 3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -5,8 +5,8 @@ const Person = require('./models/person')
 
 const app = express()
 
-app.use(express.json())
 app.use(express.static('dist'))
+app.use(express.json())
 app.use(morgan('tiny'))
 
 app.get('/api/persons', (request, response) => {
@@ -22,17 +22,24 @@ app.get('/info', (request, response) => {
     response.send(output)
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then((person) => {
-        response.json(person)
-    })
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id;
-    persons = persons.filter(p => p.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -63,10 +70,24 @@ app.post('/api/persons', (request, response) => {
         console.log(savedPerson)
         response.json(savedPerson)
     })
-
 })
 
-const generateId = () => String(Math.floor(Math.random() * 10000000))
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
 
-const PORT = 3001
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
