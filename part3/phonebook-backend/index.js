a594a5443e4041f66e2a1978a8ba922b5dc712e1
stefano76 <stefano.bonuccelli@gmail.com>
2026-09-03 17:33:33 +1200

    Exercise 3.17*: Phonebook database, step 5

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -65,6 +65,25 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const { name, number } = request.body
+
+    Person.findById(request.params.id)
+        .then(person => {
+            if (!person) {
+                return response.status(404).end()
+            }
+
+            person.name = name
+            person.number = number
+
+            return person.save().then((updatedPerson) => {
+                response.json(updatedPerson)
+            })
+        })
+        .catch(error => next(error))
+})
+
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
