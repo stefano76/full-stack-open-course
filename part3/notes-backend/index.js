3878ff7606f0fe86ae9609051adeedbb701a5166
stefano76 <stefano.bonuccelli@gmail.com>
2026-09-06 10:13:22 +1200

    Added Mongoose validation to Notes backend

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -48,7 +48,7 @@ app.delete('/api/notes/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
 
     if ( !body.content ) {
@@ -60,9 +60,11 @@ app.post('/api/notes', (request, response) => {
         important: body.important || false
     })
 
-    note.save().then(savedNote => {
-        response.json(savedNote)
-    })
+    note.save()
+        .then(savedNote => {
+            response.json(savedNote)
+        })
+        .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
@@ -95,6 +97,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
